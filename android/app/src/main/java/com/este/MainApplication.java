7346270b86316bbde1906ec1ae4729bae1f6a257
Matěj Kříž <matej.kriz@actum.cz>
2017-04-21 12:23:51 +0200

    unlinked all packages in preparation for upgrade

--- android/app/src/main/java/com/este/MainApplication.java
+++ android/app/src/main/java/com/este/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.i18n.reactnativei18n.ReactNativeI18n;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
